0131c70e02667cd0a7e7257b09fce26a056438f5
Michael Hanely <michael.hanley@mycit.ie>
2022-05-08 19:02:51 +0100

    commit

--- serviceWorker.js
+++ serviceWorker.js
@@ -36,18 +36,14 @@ self.addEventListener('fetch', (e) => {
         console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
         if (r) { return r; }
 
-        try {
-            const response = await fetch(e.request);
-            const cache = await caches.open(cacheName);
-            console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-            cache.put(e.request, response.clone());
-            return response;
 
-        } catch (error) {
-            return response;
+        const response = await fetch(e.request);
+        const cache = await caches.open(cacheName);
+        console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+        cache.put(e.request, response.clone());
+        return response;
 
 
-        }
 
 
     }));
