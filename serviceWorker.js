66251d035e700938056faec43b42cdba44de6cb7
Michael Hanely <michael.hanley@mycit.ie>
2022-05-08 19:49:19 +0100

    commit

--- serviceWorker.js
+++ serviceWorker.js
@@ -2,14 +2,15 @@
 
 
 const cacheName = 'PWAa03';
+
 const appShellFiles = [
-    '/',
-    '/index.html',
-    '/index2.html',
-    '/myWorker.js',
-    '/style.css',
-    'icons/white.png',
-    '/favicon.png',
+    '/PWA_A03/',
+    '/PWA_A03/index.html',
+    '/PWA_A03/index2.html',
+    '/PWA_A03/myWorker.js',
+    '/PWA_A03/style.css',
+    '/PWA_A03icons/white.png',
+    '/PWA_A03/favicon.png',
 
 ];
 
