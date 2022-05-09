23e40e212a717e965b04cc3a5d0bd454afe5bad0
Michael Hanely <michael.hanley@mycit.ie>
2022-05-09 13:19:32 +0100

    commit

--- myWorker.js
+++ myWorker.js
@@ -3,34 +3,50 @@ onmessage = function (e) {
     importScripts("movieObj.js");
 }
 function processFilms(data) {
-    var checkedFilms = 0
-    var filmScriptArray = Object.keys(data).map(key => {
-        return data[key];
-    })
-    var totalFilms = filmScriptArray.length
+    console.log(data)
+    try {
+        if (data.offline == true) {
+            messageObj = {
+                checkedFilms: 0,
+                filmScriptObjFormatted: null,
+                totalFilms: 0,
+                offline: true
 
-    for (const filmScriptObj of filmScriptArray) {
-        checkedFilms++
-        messageObj = {
-            checkedFilms: checkedFilms,
-            filmScriptObjFormatted: null,
-            totalFilms: totalFilms
-        }
-
-
-
-        //  console.log(checkedFilms, '/', filmScriptArray.length)
-        if (filmScriptObj.title.toUpperCase().includes(searchTerm.toUpperCase())) {
-
-
-
-            var regex = new RegExp(searchTerm, 'ig');
-            titleMarked = filmScriptObj.title.replace(regex, '<mark>$&</mark>');
-            let filmScriptObjFormatted = { title: titleMarked, URL: filmScriptObj.link }
-            messageObj.filmScriptObjFormatted = filmScriptObjFormatted
-
+            }
         }
         postMessage(messageObj)
 
+    }
+    catch {
+        var checkedFilms = 0
+        var filmScriptArray = Object.keys(data).map(key => {
+            return data[key];
+        })
+        var totalFilms = filmScriptArray.length
+
+        for (const filmScriptObj of filmScriptArray) {
+            checkedFilms++
+            messageObj = {
+                checkedFilms: checkedFilms,
+                filmScriptObjFormatted: null,
+                totalFilms: totalFilms
+            }
+
+
+
+            //  console.log(checkedFilms, '/', filmScriptArray.length)
+            if (filmScriptObj.title.toUpperCase().includes(searchTerm.toUpperCase())) {
+
+
+
+                var regex = new RegExp(searchTerm, 'ig');
+                titleMarked = filmScriptObj.title.replace(regex, '<mark>$&</mark>');
+                let filmScriptObjFormatted = { title: titleMarked, URL: filmScriptObj.link }
+                messageObj.filmScriptObjFormatted = filmScriptObjFormatted
+
+            }
+            postMessage(messageObj)
+
+        }
     }
 }
