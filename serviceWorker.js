6b577c99ac386d1b14a6daf76b9bef76aff27bdc
Michael Hanely <michael.hanley@mycit.ie>
2022-05-09 11:19:06 +0100

    commit

--- serviceWorker.js
+++ serviceWorker.js
@@ -13,13 +13,13 @@ const appShellFiles = [
 
 ];
 
-var flickImage = []
+//var flickImage = []
 
 self.addEventListener('install', (e) => {
     console.log('[Service Worker] Install');
     e.waitUntil((async () => {
         const cache = await caches.open(cacheName);
-        console.log('[Service Worker] Caching all: app shell');
+        // console.log('[Service Worker] Caching all: app shell');
         console.log(cache);
         await cache.addAll(appShellFiles);
     })());
@@ -32,14 +32,15 @@ self.addEventListener('activate', e => {
 
 
 self.addEventListener('fetch', (e) => {
+    console.log(e)
 
     e.respondWith((async () => {
         const r = await caches.match(e.request);
-        console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+        //  console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
         if (r) { return r; }
         const response = await fetch(e.request);
         const cache = await caches.open(cacheName);
-        console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+        //  console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
         cache.put(e.request, response.clone());
         return response;
 
