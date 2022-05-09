1978aafb593409d226c87ac1582c3c7ce2faade9
Michael Hanely <michael.hanley@mycit.ie>
2022-05-09 15:40:45 +0100

    commit

--- serviceWorker.js
+++ serviceWorker.js
@@ -24,11 +24,21 @@ self.addEventListener('install', (e) => {
     })());
 });
 
-self.addEventListener('activate', e => {
-
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function (cacheName) {
+                    // Return true if you want to remove this cache,
+                    // but remember that caches are shared across
+                    // the whole origin
+                }).map(function (cacheName) {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );
 });
-
-
 
 self.addEventListener('fetch', (e) => {
     var reqURL = e.request.url.toString()
